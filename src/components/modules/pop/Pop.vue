23aca23a2cbc563eda5f935ca7c2569905392802
zhangjintong <947140669@qq.com>
2020-11-25 17:56:10 +0800

    添加pop组件

---
+++ src/components/modules/pop/Pop.vue
@@ -0,0 +1,36 @@
+<template>
+  <div v-if="isShow" class="tips animation">{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Pop',
+  data () {
+    return {
+
+    }
+  },
+  props: {
+    type: {
+      type: String,
+      default: ''
+    },
+    msg: {
+      type: String,
+      default: ''
+    },
+    isShow: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scope>
+.tips {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+}
+</style>
